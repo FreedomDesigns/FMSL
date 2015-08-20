f5dd1b828a1768b85a8106b69f0bd3c394f772b0
Alex King <ukcyclone14@gmail.com>
2015-08-20 01:31:00 +0100

    Small Update To Product Info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InfinityLogger")]
+[assembly: AssemblyTitle("FMSL")]
 [assembly: AssemblyDescription("This is a .DLL extension that provides console colour logging")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alex King")]
-[assembly: AssemblyProduct("InfinityLogger")]
+[assembly: AssemblyCompany("Freedom [Alex]")]
+[assembly: AssemblyProduct("FMSL")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

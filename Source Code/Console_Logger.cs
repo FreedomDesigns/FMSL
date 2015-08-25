de4cb5b20e02d9f33e91e9de6c5fc0312cf2b185
Alex King <ukcyclone14@gmail.com>
2015-08-25 01:32:21 +0100

    Small Update
    Log Types/Flags

--- Source Code/Console_Logger.cs
+++ Source Code/Console_Logger.cs
@@ -5,11 +5,13 @@
 	Purpose: Lightweight and Basic Logger
 
 	Initial author: Freedom Designs
+    "Never Judge A Coder By Their Code."
 	Started: 2015-08-19
 	===========================================
     NOTE: This Is Not Finish And Will Contain Bug's Or Issue's.
 */
 
+using Freedom_Logger;
 using System;
 using System.IO;
 
@@ -17,64 +19,70 @@ namespace FMSL
 {
     public class Console_Logger
     {
-        // This allows the user to externally set if a log file is created. 
-        public static bool Log_To_File { private get; set; }
+        private static string Path_Logged = @"m3demo";  // Sets Default Path For The Storage Of Logs.
+        private static DateTime NOW = DateTime.Now;     // Gets Time Of Current PC/User.
 
-        private static string Path_Logged = @"m3demo";
-        public static string Log_Path { private get { return Path_Logged; } set { Path_Logged = value; } }
+        // This allows the user to externally set select options. 
+        public static bool Log_To_File { private get; set; }                                                // Allows The User To Create Log Files Of Messages That Pass Though This [Default = FALSE].
+        public static string Log_Path { private get { return Path_Logged; } set { Path_Logged = value; } }  // Allows The User To Change The Location Of Where Log Files Are Stored [Default = FALSE].
 
-        private static DateTime NOW = DateTime.Now;
-        private static string Log_Name = string.Format("{0}/{1:MM-dd-yy_H.mm.ss}.log",Path_Logged ,NOW);
+        // TODO: Have a look at changing the time format [Removing the seconds].
+        private static string Log_Name = string.Format("{0}/{1:MM-dd-yy_H.mm}.log", Path_Logged, NOW); // Sets The Name Of Log Files.
 
-        public static void ConsoleColour(string Message, int Type = 0)
+        // Type Numbers
+        // ------------
+        // Low Importance - Cyan
+        // Warning - Yellow
+        // Error - Red
+        // Debug - Green
+        // Info - Magenta
+
+        public static void ConsoleColour(string Message, LogLevel Level)
         {
-            // Type Numbers
-            // 0 = Low Importance - Cyan
-            // 1 = Warning - Yellow
-            // 2 = Error - Red
-            // 3 = Debug - Green
-            // 4 = Info - Magenta
+            string LogLine = null;
 
             if (Message != null)
             {
-                if (Type == 0)
+                switch (Level)
                 {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    LogWrite(string.Format("{0} - {1} {2}", NOW, "Notice:", Message));
-                }
-                else if (Type == 1)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    LogWrite(string.Format("{0} - {1} {2}", NOW, "Warning:", Message));
-                }
-                else if (Type == 2)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    LogWrite(string.Format("{0} - {1} {2}", NOW, "Error:", Message));
-                }
-                else if (Type == 3)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    LogWrite(string.Format("{0} - {1} {2}", NOW, "Debug:", Message));
-                }
-                else if (Type == 4)
-                {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    LogWrite(string.Format("{0} - {1} {2}", NOW, "Info:", Message));
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    LogWrite(string.Format("{0} - {1} {2}", NOW, "Error:", "Invalid Number"));
+                    case LogLevel.Low:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        LogLine = string.Format("{0} - {1} {2}", NOW, "Notice:", Message);
+                        break;
+                    case LogLevel.Warning:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        LogLine = string.Format("{0} - {1} {2}", NOW, "Warning:", Message);
+                        break;
+                    case LogLevel.Error:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        LogLine = string.Format("{0} - {1} {2}", NOW, "Error:", Message);
+                        break;
+                    case LogLevel.Debug:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        LogLine = string.Format("{0} - {1} {2}", NOW, "Debug:", Message);
+                        break;
+                    case LogLevel.Info:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        LogLine = string.Format("{0} - {1} {2}", NOW, "Info:", Message);
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        LogLine = string.Format("{0} - {1} {2}", NOW, "Error:", "Invalid Number");
+                        break;
                 }
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                LogWrite(string.Format("{0} - {1} {2}", NOW, "Error:", "Um You Need To Give Me Text!"));
+                LogLine = string.Format("{0} - {1} {2}", NOW, "Error:", "Um You Need To Give Me Text!");
             }
+            LogWrite(LogLine);
             Console.ResetColor();
         }
+
+        /* ----------------------------------------------------------
+           * All Threads Below Should Not Display Any Console Text. *
+           ---------------------------------------------------------- */
 
         private static void LogWrite(string Message)
         {
@@ -84,7 +92,6 @@ namespace FMSL
             // 2. Check Is User Wants Message Logged, If So Send To The Right Thread.
             if (Log_To_File == true)
             {
-                Console.WriteLine("I'm Loved ^^");
                 WriteToFile(Message);
             }
         }
@@ -100,16 +107,13 @@ namespace FMSL
             // 2. Check If Log File Exists
             if (!File.Exists(Log_Name))
             {
-                Console.WriteLine(Log_Name);
                 File.CreateText(Log_Name);
             }
 
-            using (StreamWriter sw = File.AppendText(Log_Name))
-            {
-                DateTime NOW = DateTime.Now;
-                sw.WriteLine(Message);
-                sw.Close();
-            }
+            // 3. Write To File
+            StreamWriter sw = new StreamWriter(Log_Name, true);
+            sw.WriteLine(Message);
+            sw.Close();
         }
     }
 }
---
+++ Source Code/LogLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Freedom_Logger
+{
+    public enum LogLevel
+    {
+            Low = 0,
+            Warning = 1,
+            Error = 2,
+            Debug = 4,
+            Info = 8,
+    } 
+}
